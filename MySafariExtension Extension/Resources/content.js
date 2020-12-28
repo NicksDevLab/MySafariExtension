123638cdd22bd6de9e3ed014464ad0eb6f4f68de
NicksDevLab <nicholaschurch@ymail.com>
2020-12-28 06:16:17 -0500

    no message

--- MySafariExtension Extension/Resources/content.js
+++ MySafariExtension Extension/Resources/content.js
@@ -1,19 +1,28 @@
 
+
 let selectedSpeed;
-let readingSpeed;
-let intervalId;
-let readerModeOn = false;
-let articleFound = false;
-let wordsArray = [];
+let scrollSpeed;
+let scrollInterval;
+
 let paragraphs = [];
+let wordsArray = [];
+
 let paraIndex = 0;
 let wordIndex = 0;
-let mouseDown = false;
+
+let isReaderModeOn = false;
+let isArticleFound = false;
+let isMouseDown = false;
+
+
+document.addEventListener('visibilitychange', function() {
+    if (isReaderModeOn) { turnReaderModeOff(); }
+});
 
 
 browser.runtime.onMessage.addListener(message => {
-      selectedSpeed = message.speed;
-      readerModeOn = message.isActive;
+    selectedSpeed = message.speed;
+    isReaderModeOn = message.isActive;
     if (window.top === window) {
       toggleReaderMode();
     }
@@ -21,7 +30,7 @@ browser.runtime.onMessage.addListener(message => {
 
 
 function toggleReaderMode() {
-    if (readerModeOn) {
+    if (isReaderModeOn) {
         setSpeed(selectedSpeed);
         turnReaderModeOn();
     } else {
@@ -31,12 +40,13 @@ function toggleReaderMode() {
 
 
 function turnReaderModeOff() {
-    clearInterval(intervalId);
+    clearInterval(scrollInterval);
     document.getElementById("background").remove();
-    readerModeOn = false;
-    articleFound = false;
+    isReaderModeOn = false;
+    isArticleFound = false;
     wordsArray = [];
     paragraphs = [];
+    browser.runtime.sendMessage({ isActive: false });
 }
 
 
@@ -44,21 +54,22 @@ function setSpeed(wpm) {
     const minute = 60_000;
     const speed = Number(wpm);
     const perWord = minute / speed;
-    readingSpeed = Math.round(perWord / 5);
+    scrollSpeed = Math.round(perWord / 5);
 }
 
 
 function turnReaderModeOn() {
     getArticleText(document.body);
     setBackground();
-    intervalId = setInterval(beginScroll, readingSpeed * 5);
-    readerModeOn = true;
+    scrollInterval = setInterval(beginScroll, scrollSpeed * 5);
+    isReaderModeOn = true;
 }
 
 
-const name = Object.freeze({
+const idProperty = Object.freeze({
     id: "id",
-    className: "className"
+    className: "className",
+    itemProp: "itemprop"
 })
 
 
@@ -70,19 +81,36 @@ function getArticleText(section) {
     switch (document.location.host) {
         case "news.yahoo.com":
             conditionsToCheck = ["caas-body"];
-            idMethod = name.className;
+            idMethod = idProperty.className;
             break;
         case "www.npr.org":
             conditionsToCheck = ["storytext"];
-            idMethod = name.id;
+            idMethod = idProperty.id;
             break;
         case "www.nbcnews.com":
             conditionsToCheck = ["article-body__content"];
-            idMethod = name.className;
+            idMethod = idProperty.className;
             break;
         case "quillette.com":
             conditionsToCheck = ["entry-content"];
-            idMethod = name.className;
+            idMethod = idProperty.className;
+            break;
+        case "www.foxnews.com":
+            conditionsToCheck = ["article-body"];
+            idMethod = idProperty.className;
+            break;
+        case "www.huffpost.com":
+            conditionsToCheck = ["entry-text"];
+            idMethod = idProperty.id;
+            break;
+        case "www.cnn.com":
+            conditionsToCheck = ["l-container"];
+            idMethod = idProperty.className;
+            break;
+        case "www.dailymail.co.uk":
+            conditionsToCheck = ["articleBody"];
+            idMethod = idProperty.itemProp;
+            break;
         default:
             break;
     }
@@ -91,71 +119,98 @@ function getArticleText(section) {
 
 
 function getArticle(section, conditions, idMethod) {
-console.log("GET ARTICLE");
+
     for (let i = 0; i < section.childNodes.length; i++) {
 
         let label = "";
-
+        console.log("NODENAME - ", section.nodeName);
         switch (idMethod) {
-            case name.id:
+            case idProperty.id:
                 label = section.childNodes[i].id;
-            case name.className:
+                break;
+            case idProperty.className:
                 label = section.childNodes[i].className;
+                break;
+            case idProperty.itemProp:
+                if (section.hasAttribute("itemprop")) {
+                    label = section.getAttribute("itemprop");
+                } 
+                break;
         }
 
         for (let j = 0; j < conditions.length; j++) {
 
             if (conditions[j] === label) {
-                articleFound = true;
-                filterNonSense(section.childNodes[i]);
+                isArticleFound = true;
+                console.log("*****", section.childNodes);
+                if (document.location.host == "www.dailymail.co.uk") {
+                    for (let k = 0; k < section.childNodes.length; k++) {
+                        findParagraphs(section.childNodes[k]);
+                    }
+                } else {
+                    for (let k = 0; k < section.childNodes[i].childNodes.length; k++) {
+                        findParagraphs(section.childNodes[i].childNodes[k]);
+                    }
+                }
             }
         }
 
         if (section.childNodes[i].childNodes.length > 0) {
             getArticle(section.childNodes[i], conditions, idMethod);
-            if (articleFound) { break; }
+            if (isArticleFound) { break; }
+        }
+    }
+}
+
+function findParagraphs(element) {
+    if (document.location.host == "www.cnn.com") {
+        if (element.className == "zn-body__paragraph") {
+            extractText(element);
+        }
+    } 
+    else if (element.nodeName == "P") {
+        extractText(element);
+    } 
+
+    if ((element.childNodes.length > 0) && 
+        ((document.location.host == "www.huffpost.com") ||
+         (document.location.host == "www.cnn.com") || 
+         (document.location.host == "www.dailymail.co.uk"))) {
+        for (let i = 0; i < element.childNodes.length; i++) {
+            findParagraphs(element.childNodes[i]);
         }
     }
 }
 
 
-function filterNonSense(section) {
-    console.log("FILTER NONSENSE");
-    for (let i = 0; i < section.childNodes.length; i++) {
-
-        if (section.childNodes[i].nodeName == "P") {
-
-            let text = section.childNodes[i].innerText;
-
-            let word = ""
-            for (let j = 0; j < text.length; j++) {
-                if ((text[j] == " ") || (text[j] == "\n") || (text[j] == ".")) {
-                    if (word.length > 0) {
-                        wordsArray.push(word);
-                        word = "";
-                    }
-                } else {
-                    word += text[j];
+function extractText(element) {
+    let text = element.innerText;
+        let array = [];
+        let word = "";
+        for (let l = 0; l < text.length; l++) {
+            if ((text[l] == " ") || (text[l] == "\n") || (text[l] == ".")) {
+                if (word.length > 0) { 
+                    array.push(word);
+                    word = "";
                 }
+            } else {
+                word += text[l];
             }
-            console.log("PARA - ", wordsArray);
-            paragraphs.push(wordsArray);
-            wordsArray = [];
         }
-    }
+        paragraphs.push({
+            wordArray: array,
+            yPosition: element.offsetTop 
+        });
 }
-
 
 function setBackground() {
     const backgroundDiv = document.createElement('h1');
     backgroundDiv.id = "background";
     backgroundDiv.onmousedown = function() {
-        mouseDown = true;
-        console.log("MOUSE DOWN")
+        isMouseDown = true;
     };
     backgroundDiv.onmouseup = function() {
-        mouseDown = false;
-        console.log("MOUSE UP")
+        isMouseDown = false;
     };
     document.body.appendChild(backgroundDiv);
 }
@@ -163,20 +218,30 @@ function setBackground() {
 
 function beginScroll() {
 
-     if (!mouseDown) { return; }
-     
-     if (wordIndex >= paragraphs[paraIndex].length) { 
+     if (!isMouseDown) { return; }
+
+     if (paragraphs.length == 0) {
+        document.getElementById("background").innerText = "Unable to detect article text";
+        return;
+     }
+
+     if (wordIndex >= paragraphs[paraIndex].wordArray.length) { 
          paraIndex += 1;
          wordIndex = 0;
+         scrollTo({
+             top: paragraphs[paraIndex].yPosition,
+             left: 0, 
+             behavior: 'smooth' 
+             });
      }
 
      if (paraIndex >= paragraphs.length) { return; }
      
-     if (paragraphs[paraIndex][wordIndex].length < 5) {
-         document.getElementById("background").innerText =  paragraphs[paraIndex][wordIndex] + " " + paragraphs[paraIndex][wordIndex + 1];
+     if (paragraphs[paraIndex].wordArray[wordIndex].length < 5) {
+         document.getElementById("background").innerText =  paragraphs[paraIndex].wordArray[wordIndex] + " " + paragraphs[paraIndex].wordArray[wordIndex + 1];
          wordIndex += 1;
      } else {
-         document.getElementById("background").innerText =  paragraphs[paraIndex][wordIndex];
+         document.getElementById("background").innerText =  paragraphs[paraIndex].wordArray[wordIndex];
      }
      wordIndex += 1;
 }
